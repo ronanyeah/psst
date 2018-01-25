7b83504b073cf06af2359ab417d97140e304d5f4
Rónán <ronan_mccabe@protonmail.com>
2018-01-25 20:48:39 +0000

    start rewrite

--- src/helpers.js
+++ src/helpers.js
@@ -1,22 +1,20 @@
 // ArrayBuffer -> String
-function arrayBufferToString (buffer) {
-  return new Uint8Array(buffer).reduce(
-    function (acc, val) {
-      return acc + String.fromCharCode(val)
-    },
-    ''
-  )
+function arrayBufferToString(buffer) {
+  return new Uint8Array(buffer).reduce(function(acc, val) {
+    return acc + String.fromCharCode(val);
+  }, "");
 }
 
 // String -> ArrayBuffer
-function stringToArrayBuffer (string) {
+function stringToArrayBuffer(string) {
   return new Uint8Array(
-    string.split('')
-    .map(function (x) { return x.charCodeAt(0) })
-  )
+    string.split("").map(function(x) {
+      return x.charCodeAt(0);
+    })
+  );
 }
 
 module.exports = {
   arrayBufferToString: arrayBufferToString,
   stringToArrayBuffer: stringToArrayBuffer
-}
+};
--- test/index.js
+++ test/index.js
@@ -1,48 +1,46 @@
-const puppeteer = require('puppeteer')
-const assert = require('assert')
-
-;(async () => {
+const puppeteer = require("puppeteer");
+const assert = require("assert");
+(async () => {
   // sandbox issues: https://github.com/GoogleChrome/puppeteer/issues/290#issuecomment-322851507
-  const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
-  const a = await browser.newPage()
+  const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+  const a = await browser.newPage();
 
-  await a.goto('http://localhost:8080/')
-  await a.waitFor('.start-circle')
+  await a.goto("http://localhost:8080/");
+  await a.waitFor(".start-circle");
 
-  await a.click('.start-circle')
+  await a.click(".start-circle");
 
-  await a.waitFor('.chat-link')
+  await a.waitFor(".chat-link");
 
-  const link = await a.$eval('.chat-link', x => x.innerText)
+  const link = await a.$eval(".chat-link", x => x.innerText);
 
-  const b = await browser.newPage()
-  await b.goto(link)
+  const b = await browser.newPage();
+  await b.goto(link);
 
-  await b.waitFor('#messages')
-  await a.waitFor('#messages')
+  await b.waitFor("#messages");
+  await a.waitFor("#messages");
 
-  await a.focus('.message-input')
-  await a.type('ronan ☘️')
+  await a.focus(".message-input");
+  await a.type("ronan ☘️");
 
-  await b.waitFor('.typing')
+  await b.waitFor(".typing");
 
-  await a.click('.send-message')
+  await a.click(".send-message");
 
-  await b.waitFor('.message')
+  await b.waitFor(".message");
 
-  assert(await b.$eval('.message', x => x.innerText === 'ronan ☘️'))
+  assert(await b.$eval(".message", x => x.innerText === "ronan ☘️"));
 
-  await b.close()
+  await b.close();
 
-  await a.focus('.message-input')
-  await a.type('x')
-  await a.click('.send-message')
+  await a.focus(".message-input");
+  await a.type("x");
+  await a.click(".send-message");
 
-  await a.waitFor('.conn-lost')
+  await a.waitFor(".conn-lost");
 
-  return browser.close()
-})()
-.catch(err => {
-  console.error(err)
-  process.exit(1)
-})
+  return browser.close();
+})().catch(err => {
+  console.error(err);
+  return process.exit(1);
+});
